59ba80bcdad3292d80059220e5e47f9ef6a85b03
MonsteParty23 <lorelyn.paga@gmail.com>
2025-06-01 18:15:34 +0800

    initial commit

--- project.js
+++ project.js
@@ -38,5 +38,20 @@ const getNumberOfLines = () =>{
     }
   } 
 };
-const depositAmount = deposit();
+// ===== 3. Collect a Bet Amount =====
+const getBet = (balance) => {
+     while(true){
+    const bet = prompt("Enter the total bet: "); 
+    const numberBet = parseFloat(bet);
+
+    if(isNaN(numberBet) || numberBet <= 0 || numberBet > balance){
+        console.log("Invalid bet, try again.");
+    } else{
+        return numberBet; 
+    }
+  } 
+};
+
+let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const bet = getBet(balance);
