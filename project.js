b7b0e0cf0ed5317e6460e12d6a21763b76df3bcd
MonsteParty23 <lorelyn.paga@gmail.com>
2025-06-03 09:51:28 +0800

    initial commit

--- project.js
+++ project.js
@@ -84,13 +84,17 @@ const spin = () =>{
             const reelSymbols = [...symbols];
             for(let j = 0; j < ROWS; j++){
                 const randomIndex = Math.floor(Math.random() * reelSymbols.length);
-                const selectedSymbol = reelSymbols[randomIndexd];
+                const selectedSymbol = reelSymbols[randomIndex];
+                reels[i].push(selectedSymbol);
+                reelSymbols.splice(randomIndex, 1); //remove the selected symbol from the array) 
             }
         }
+
+        return reels;
     };
 
-    spin();
-
+const reels = spin();
+console.log(reels);
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
